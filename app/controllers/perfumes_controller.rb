60ac29ac0d078fce2409c10c65a053eb7313456f
githubのユーザー名 <github>
2026-03-14 16:32:58 +0900

    (createの変更2)

--- app/controllers/perfumes_controller.rb
+++ app/controllers/perfumes_controller.rb
@@ -14,10 +14,11 @@ class PerfumesController < ApplicationController
         @perfume = Perfume.new(perfume_params)
         if @perfume.save
             flash[:notice] = "診断が完了しました"
-            redirect_to @perfume
-           # redirect_to perfume_path(perfume.id)
+            #redirect_to @perfume
+            redirect_to perfume_path(perfume.id)
         else
-            redirect_to :action => "new"
+            #redirect_to :action => "new"
+            render :new
         end
     end
   
